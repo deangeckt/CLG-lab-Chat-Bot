41262db2b5b1afa826de758b7e64bd8a8b4560cc
Dean Geckt <t-deangeckt@microsoft.com>
2022-09-23 14:49:27 +0300

    clean images

--- client/src/common/Header.tsx
+++ client/src/common/Header.tsx
@@ -4,7 +4,7 @@ import './Header.css';
 function Header(): JSX.Element {
     return (
         <div className="Header">
-            <img className="clg_logo" src={'clg_logo.png'} />
+            <img className="clg_logo" src={'clg_logo.jpg'} />
             <img className="haifa_logo" src={'haifa_logo.jpg'} />
         </div>
     );
