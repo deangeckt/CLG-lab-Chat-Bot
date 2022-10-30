c20e674719cf11222d29bf3a362e3cc0555b7a68
Dean Geckt <t-deangeckt@microsoft.com>
2022-10-30 15:56:53 +0200

    fix chat scroll bug

--- client/src/map_task/useMapCanvas.tsx
+++ client/src/map_task/useMapCanvas.tsx
@@ -6,8 +6,8 @@ import { MapCellIdx } from '../Wrapper';
 import { useApp } from './useApp';
 
 const get_canvas_size = (im_width: number, im_height: number) => {
-    // map canvas is 75% width
-    const container_width = 0.75 * 0.9 * window.innerWidth;
+    // map canvas is 75% width and app container height is 0.9
+    const container_width = 0.75 * window.innerWidth;
     const container_height = 0.75 * 0.9 * window.innerHeight;
 
     const ratio_w = container_width / im_width;
