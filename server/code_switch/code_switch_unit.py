9291ce2421cea408328f5fd94d3cbac72815e977
Be-better-Do-better <netzerbaram@gmail.com>
2022-12-08 15:55:56 +0200

    adding the option for inter-sentential cs in the cs unit

--- server/code_switch/code_switch_unit.py
+++ server/code_switch/code_switch_unit.py
@@ -1,4 +1,11 @@
 import json
+import random
+import langid
+
+from utils import *
+
+LANG_CODES = {'en': 'eng', 'es': 'spa'}
+langid.set_languages(langs=['es', 'en'])
 
 
 class CodeSwitchUnit:
@@ -9,6 +16,9 @@ class CodeSwitchUnit:
 
         self.cs_history = []
         self.current_cs_state = None
+        self.len_of_current_subsequence = 1
+
+        self.user_msg = None
 
     def call(self, user_msg: str, en_bot_resp: list[str]):
         """
@@ -16,38 +26,82 @@ class CodeSwitchUnit:
         param en_bot_resp: the generated messages (list) the bot generated in english
         :return: spanglish generated string in a list
         """
+        self.user_msg = user_msg
+        self.identify_incoming_cs_state()
+        new_cs_state = self.predict_next_cs_state()
+        self.update_cs_state(new_cs_state)
+        # HERE GOES THE TRANSLATION OF THE CHATBOT's RESPONSE!!!
         return en_bot_resp
 
+    def identify_incoming_cs_state(self):
+        identified_lang_langid = langid.classify(self.user_msg)[0]
+        self.current_cs_state = LANG_CODES[identified_lang_langid]
+
     def predict_next_cs_state(self):
-        if self.current_cs_state == None:  #not-initialized
-            pass
+        if self.current_cs_state is None:  # not-initialized
+            new_state = self.random_strategy()
 
         elif self.cs_strategy == 'goldfish':
-            pass
+            new_state = self.goldfish_cs_strategy()
 
         elif self.cs_strategy == 'random':
-            pass
+            new_state = self.random_strategy()
 
         else:
-            pass
+            new_state = self.random_strategy()
+
+        return new_state
 
     def update_cs_state(self, new_state):
         self.current_cs_state = new_state
         self.cs_history.append(new_state)
 
     def goldfish_cs_strategy(self):
-        pass
+        r = self.params.states_options_dict[self.current_cs_state].r
+        p = hazard(r, s=self.len_of_current_subsequence)
+        res = random.choices([True, False], [p, 1 - p])[0]
+        if res:  # change state
+            next_state = self.select_next_state(self.current_cs_state)
+            self.len_of_current_subsequence = 1
+        else:  # keep current state
+            next_state = self.current_cs_state
+            self.len_of_current_subsequence += 1
+        return next_state
+
+    def select_next_state(self, current_state):
+        all_next_possible_state_options = []
+        all_next_possible_state_probabilities = []
+        for next_state_option, next_state_transition_probability in \
+                self.params.states_options_dict[current_state].transitions.items():
+
+            if not(next_state_option == current_state):
+                all_next_possible_state_options.append(next_state_option)
+                all_next_possible_state_probabilities.append(next_state_transition_probability)
+        next_state = random.choices(all_next_possible_state_options, all_next_possible_state_probabilities)[0]
+        return next_state
 
     def random_strategy(self):
-        pass
+        weights = []
+        lang_options = self.params.state_options
+        for lang in lang_options:
+            cs_option = self.params.states_options_dict[lang]
+            p = cs_option.p
+            weights.append(p)
+        return random.choices(lang_options, weights)[0]
 
 
 def test_codeswitchunit():
     param_file_name = "cs_parameters.txt"
     cs_strategy = "goldfish"
     csunit = CodeSwitchUnit(param_file_name, cs_strategy)
+
     csunit.predict_next_cs_state()
-    print(csunit.current_cs_state)
+    for _ in range(3):
+        user_msg = "me gusta comer sushi"
+        csunit.call(user_msg, en_bot_resp="nice!")
+        user_msg = "it is nice to live in america"
+        csunit.call(user_msg, en_bot_resp="nice!")
+    print(csunit.cs_history)
 
 
 class CSOption(object):
@@ -67,8 +121,8 @@ class CSOption(object):
 class CSParameters(object):
     def __init__(self, param_file_name):
         self.param_file_name = param_file_name
-
-        self.states_options = {}
+        self.state_options = []
+        self.states_options_dict = {}
         self.load_parameters()
 
     def load_parameters(self):
@@ -76,7 +130,7 @@ class CSParameters(object):
             variables = json.load(f)
 
         for lang, params in variables.items():
-
+            self.state_options.append(lang)
             transitions = {}
             for param_name, param in params.items():
                 if param_name == "p":
@@ -86,16 +140,15 @@ class CSParameters(object):
                 else:
                     transitions[param_name] = param
 
-            self.states_options[lang] = CSOption(
+            self.states_options_dict[lang] = CSOption(
                 state_name=lang, p=p, transitions=transitions, r=r)
 
 
 def test_csparameters():
     param_file_name = "cs_parameters.txt"
     params = CSParameters(param_file_name)
-    for cs_state, cs_params in params.states_options.items():
+    for cs_state, cs_params in params.states_options_dict.items():
         print(cs_params)
-
 
 
 def run_tests():
