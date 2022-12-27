0fdcf659aeea33827a6ed90801fe6e96a6401560
Dean Geckt <t-deangeckt@microsoft.com>
2022-12-27 14:55:03 +0200

    fix merge

--- server/tests/local_bot.py
+++ server/tests/local_bot.py
@@ -4,9 +4,6 @@ from google_cloud.translate import Translate
 Translate.__wrapped__.translate_to_eng = lambda self, x: x
 Translate.__wrapped__.translate_to_spa = lambda self, x: x
 
-'''
-Notice this code use the google translation without a mock - be aware of usage consumption 
-'''
 server = BotServer("goldfish")
 guid = server.register(2)
 
