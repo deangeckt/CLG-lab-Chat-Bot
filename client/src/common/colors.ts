397d9c45d352224637838d8a8489d30683db4a97
Dean Geckt <t-deangeckt@microsoft.com>
2023-01-08 17:51:00 +0200

    revert client anim of usr map path - no need casue of new flow

--- client/src/common/colors.ts
+++ client/src/common/colors.ts
@@ -2,4 +2,3 @@ export const main_gray = '#484644';
 export const main_blue = '#3f51b5';
 export const path_cell_color = 'rgba(255, 255, 0, 0.8)';
 export const next_cells_color = 'rgba(153, 51, 255, 0.8)';
-export const flicker_color = 'rgba(255, 0, 255, 1)';
