ef37c0c56d508cfb0f984b23f72083d7f6a969aa
Dean Geckt <t-deangeckt@microsoft.com>
2023-10-02 10:39:31 +0300

    gpt bot - strip to short sentences after a dot

--- server/bots/bot.py
+++ server/bots/bot.py
@@ -44,6 +44,17 @@ class Bot(metaclass=ABCMeta):
 
     @staticmethod
     def informal_post_process(msg: str) -> list[str]:
-        msg =  msg.lower()
-        return [msg]
+        basic =  [m.strip() for m in msg.lower().split('.') if len(m)]
+        return basic
+        # end_sentence_delim = ['!', '?', '.']
+        # end_tokens_indices = [msg.rfind(token) for token in end_sentence_delim]
+        # end_tokens_indices = list(filter(lambda x: x >= 0, end_tokens_indices))
+        # if end_tokens_indices:
+        #     offset = max(end_tokens_indices) + 1
+        #     if offset == len(msg):
+        #
+        #     return [msg[:offset], msg[offset:].strip()]
+        # else:
+        #     return [msg]
+
 
